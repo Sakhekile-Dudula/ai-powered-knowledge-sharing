05412f84c82bc7a1fff3871c0fcb5a59314e0477
Sakhekile-Dudula <sakhekile.dudula@mrisoftware.com>
2025-10-27 16:30:56 +0200

    Update search placeholder to be more descriptive

--- src/components/HeaderSearch.tsx
+++ src/components/HeaderSearch.tsx
@@ -108,7 +108,7 @@ export function HeaderSearch({ accessToken }: HeaderSearchProps) {
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-slate-400" />
         <Input
           type="text"
-          placeholder="Search knowledge base..."
+          placeholder="Search documents, discussions, code snippets, and guides..."
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           onFocus={() => searchQuery && setIsOpen(true)}
